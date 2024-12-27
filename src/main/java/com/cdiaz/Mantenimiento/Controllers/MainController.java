ec505422d5a89a96005c9f291da80028ea583f8a
cdiaz <cardisukirit@hotmail.com>
2024-12-27 14:57:53 -0500

    Modificacion de vista - Se visualiza usuarios

--- src/main/java/com/cdiaz/Mantenimiento/Controllers/MainController.java
+++ src/main/java/com/cdiaz/Mantenimiento/Controllers/MainController.java
@@ -1,16 +1,31 @@
 package com.cdiaz.Mantenimiento.Controllers;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MainController {
 
+    private  UsuariosController usuariosController;
+
     @GetMapping("/")
-    public String home() {
-        return "redirect:/consultas/usuarios"; // Se asume que "index.html" está en "src/main/resources/templates"
+    public String redirectHome() {
+        return "redirect:index";
     }
+
+    @GetMapping("/index")
+    public String home() {
+        return "index";
+    }
+
+   /*@GetMapping("index")
+    public String consultaUsuarios() {
+
+        return "redirect:/usuarios";
+    }*/
 }
--- src/main/java/com/cdiaz/Mantenimiento/Controllers/UsuariosController.java
+++ src/main/java/com/cdiaz/Mantenimiento/Controllers/UsuariosController.java
@@ -4,13 +4,14 @@ import com.cdiaz.Mantenimiento.Models.Usuarios;
 import com.cdiaz.Mantenimiento.Services.UsuariosRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/consultas/usuarios")
+@RequestMapping("/usuarios")
 public class UsuariosController {
     private final UsuariosRepository usuariosRepository;
 
@@ -22,6 +23,6 @@ public class UsuariosController {
     public String obtenerUsuarios(Model model){
         List<Usuarios> usuarios = usuariosRepository.findAll();
         model.addAttribute("usuarios", usuarios);
-        return "usuarios"; // Nombre del archivo en la carpeta de templates
+        return "usuarios"; //Muestra la plantilla usuarios.html
     }
 }
